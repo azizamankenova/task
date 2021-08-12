23d3580a6ef1a2bd49c75979d90142c1f8bb359c
azizamankenova <60221908+azizamankenova@users.noreply.github.com>
2021-08-12 17:04:26 +0300

    Add files via upload

--- models/construction.py
+++ models/construction.py
@@ -12,23 +12,47 @@ class Construction(models.Model):
     def default_get(self, fields):
         res = super(Construction, self).default_get(fields)
         construction_table = [(0, 0, {'sequence': 1, 'name': "Год постройки"}),
-                              (0, 0, {'sequence': 2, 'name': "Длина отдельных пролетов между осями опор"}),
-                              (0, 0, {'sequence': 3, 'name': "Ширина между перилами"}),
-                              (0, 0, {'sequence': 4, 'name': "Ширина тротуаров "}),
-                              (0, 0, {'sequence': 5, 'name': "Высота моста"}),
-                              (0, 0, {'sequence': 6, 'name': "Свободная высота моста"}),
-                              (0, 0, {'sequence': 7, 'name': "Отверстие моста"}),
-                              (0, 0, {'sequence': 8, 'name': "Отверстие пролетов"}),
-                              (0, 0, {'sequence': 9, 'name': "Высота пролетного строения"}),
-                              (0, 0, {'sequence': 10, 'name': "Длина пролетного строения"}),
-                              (0, 0, {'sequence': 11, 'name': "Ширина пролетного строения"}),
-                              (0, 0, {'sequence': 12, 'name': "Сечения пролетного строения"}),
-                              (0, 0, {'sequence': 13, 'name': "Расстояние между осями ферм"}),
-                              (0, 0, {'sequence': 14, 'name': "Электроосвещение"})]
+                              (0, 0, {'sequence': 2, 'name': "Протяженность дороги"}),
+                              (0, 0, {'sequence': 3, 'name': "Ширина проезжей части"}),
+                              (0, 0, {'sequence': 4, 'name': "Ширина тротуаров"}),
+                              (0, 0, {'sequence': 5, 'name': "Ширина пешеходных дорог"}),
+                              (0, 0, {'sequence': 6, 'name': "Ширина разделительной грунтовой полосы"}),
+                              (0, 0, {'sequence': 7, 'name': "Ширина обочины"}),
+                              (0, 0, {'sequence': 8, 'name': "Угол откоса"}),
+                              (0, 0, {'sequence': 9, 'name': "Высота насыпи"}),
+                              (0, 0, {'sequence': 10, 'name': "Дополнительные траспортные пути"}),
+                              (0, 0, {'sequence': 11, 'name': "Количество полос"}),
+                              (0, 0, {'sequence': 12, 'name': "Дорожные знаки"}),
+                              (0, 0, {'sequence': 13, 'name': "Элементы освещения"}),
+                              (0, 0, {'sequence': 14, 'name': "Зеленые насаждения"}),
+                              (0, 0, {'sequence': 15, 'name': "Дорожные покрытия"}),
+                              (0, 0, {'sequence': 16, 'name': "Электроосвещение"})]
         res.update({
             'construction_table': construction_table
         })
         return res
+
+    # @api.model
+    # def default_get(self, fields):
+    #     res = super(Construction, self).default_get(fields)
+    #     construction_table = [(0, 0, {'sequence': 1, 'name': "Год постройки"}),
+    #                           (0, 0, {'sequence': 2, 'name': "Длина отдельных пролетов между осями опор"}),
+    #                           (0, 0, {'sequence': 3, 'name': "Ширина между перилами"}),
+    #                           (0, 0, {'sequence': 4, 'name': "Ширина тротуаров "}),
+    #                           (0, 0, {'sequence': 5, 'name': "Высота моста"}),
+    #                           (0, 0, {'sequence': 6, 'name': "Свободная высота моста"}),
+    #                           (0, 0, {'sequence': 7, 'name': "Отверстие моста"}),
+    #                           (0, 0, {'sequence': 8, 'name': "Отверстие пролетов"}),
+    #                           (0, 0, {'sequence': 9, 'name': "Высота пролетного строения"}),
+    #                           (0, 0, {'sequence': 10, 'name': "Длина пролетного строения"}),
+    #                           (0, 0, {'sequence': 11, 'name': "Ширина пролетного строения"}),
+    #                           (0, 0, {'sequence': 12, 'name': "Сечения пролетного строения"}),
+    #                           (0, 0, {'sequence': 13, 'name': "Расстояние между осями ферм"}),
+    #                           (0, 0, {'sequence': 14, 'name': "Электроосвещение"})]
+    #     res.update({
+    #         'construction_table': construction_table
+    #     })
+    #     return res
 
     asset_class = fields.Char(string="Класс актива")
     obl = fields.Char(string="Область")
@@ -53,7 +77,7 @@ class TaskConstructionTable(models.Model):
     _description = 'Construction table'
 
     sequence = fields.Integer(string="№ п/п")
-    name = fields.Char(string='Наименование')
+    name = fields.Char(string='Наименование конструктивных элементов')
     quantity = fields.Integer(string="Количество")
     unit = fields.Char(string='Ед. измерения')
     description = fields.Char(string='Описание конструктивных элементов')
